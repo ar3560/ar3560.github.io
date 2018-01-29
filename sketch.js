1652cd9c97877e3fd3e9c382c2b9cac1ad8203a7
Ashna Raman <ar3560@columbia.edu>
2018-01-28 23:28:23 -0500

    ugly clock 2.0

--- sketch.js
+++ sketch.js
@@ -4,11 +4,31 @@ function setup() {
 
 function draw() {
   background('blue');
-  fill(255, 204, 0);
-  rect(20, 20, 425, 60, 15);
-  fill(200, 255, 255);
-  ellipse(200, 200, 100, 100);
-  textSize(48);
-  fill(205, 0, 205);
-  text('Ashna says hooray',30,60);
+  fill(255, 255, 255);
+
+  // Map the function second() to values from 0~400
+  rect(20, 20, 60, map(second(), 0, 60, 0, 400));
+
+  push(); // Start a new drawing state
+  scale(12, 5); // Use function hour() directly to apply scale
+  ellipse(56, 46, 55, 55);
+  pop(); // Restore original state (scale, specifically)
+
+  var h = hour();
+  fill(250, 0, 0);
+  text('Hour:\n' + h, 100, 50);
+  var h = map(h, 0, 100, 0, 50);
+  ellipse(h, 25, 25, 25);
+
+  var m = minute();
+  text('Minute:\n' + m, 100, 150)
+
+  // First change the mode to degrees (default is radians)
+  angleMode(DEGREES);
+  // Map the function minute() to values from 0~360
+  rotate(map(minute(), 0, 60, 0, 360));
+  textSize(32);
+  fill(0, 103, 204);
+  text('Ashna says Hello World', 100, 60);
 }
+  
