6d722bd252bc33d1b577ce65a107ab85d2383cfc
Ashna Raman <ar3560@columbia.edu>
2018-04-11 17:46:46 -0400

    april 11 update

---
+++ A7.2/final.js
@@ -0,0 +1,30 @@
+function setup() {
+  createCanvas(440, 800);
+
+background(100);
+
+textSize(12);
+  fill(0);
+  text('6 AM', 10, 60);
+  text('7 AM', 10, 80);
+  text('8 AM', 10, 100);
+  text('9 AM', 10, 120);
+  text('10 AM', 10, 140);
+  text('11 AM', 10, 160);
+  text('12 PM', 10, 180);
+  text('1 PM', 10, 200);
+  text('2 PM', 10, 220);
+  text('3 PM', 10, 240);
+  text('4 PM', 10, 260);
+  text('5 PM', 10, 280);
+  text('6 PM', 10, 300);
+  text('7 PM', 10, 320);
+  text('8 PM', 10, 340);
+  text('9 PM', 10, 360);
+  text('10 PM', 10, 380);
+
+stroke(0);
+    strokeWeight(0.25);
+    line(15, 60, 15, 380);
+
+}
